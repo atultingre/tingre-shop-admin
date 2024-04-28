e236b297a0dc3449ec08e0e2054dcf7fb5b2cf8c
Atul Tingre <atultingre.work@gmail.com>
2024-04-29 03:01:40 +0530

    product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,6 +1,15 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({});
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String },
+    category: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const productModel =
   mongoose.models.product || mongoose.model("product", productSchema);
