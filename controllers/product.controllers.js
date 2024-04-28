6de3b09be83145123ddaee476ebe8067430a9648
Atul Tingre <atultingre.work@gmail.com>
2024-04-29 03:21:01 +0530

    Add Product

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -2,7 +2,22 @@ import fs from "fs";
 import productModel from "../models/product.model.js";
 
 // add food item
-const addProduct = async (req, res) => {};
+const addProduct = async (req, res) => {
+  const product = new productModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+  });
+
+  try {
+    await product.save();
+    res.json({ success: true, message: "product Added", product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // all Product list
 const listProduct = async (req, res) => {};
