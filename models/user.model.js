24230d9ad6d8f74b407de923c70bf599e1e33d27
Atul Tingre <atultingre.work@gmail.com>
2024-04-29 02:27:47 +0530

    updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -5,16 +5,12 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
-      trim: true,
       lowercase: true,
-      match: /^\S+@\S+\.\S+$/,
-      minlength: 6,
     },
     password: {
       type: String,
@@ -24,17 +20,8 @@ const userSchema = new mongoose.Schema(
       type: Object,
       default: {},
     },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
   },
-  { minimize: false }
+  { minimize: false, timestamps: true }
 );
 
 const userModel = mongoose.models.User || mongoose.model("User", userSchema);
