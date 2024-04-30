4ac289bc34c4b9aa6a10314a576c7a804ed056bf
Atul Tingre <atultingre.work@gmail.com>
2024-05-01 01:33:30 +0530

    Remove products from cart

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -22,7 +22,22 @@ const addToCart = async (req, res) => {
 };
 
 // Remove items from user cart
-const removeFromCart = async (req, res) => {};
+const removeFromCart = async (req, res) => {
+  try {
+    const userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed from cart" });
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // Get user cart data
 const getCart = async (req, res) => {
