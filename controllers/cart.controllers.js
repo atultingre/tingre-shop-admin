75195fc29cfb692282c43834f5f9cb3d1f941c7b
Atul Tingre <atultingre.work@gmail.com>
2024-05-01 01:18:07 +0530

    Add Products to Cart

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -1,5 +1,27 @@
+import userModel from "../models/user.model.js";
+
 // add items to user cart
-const addToCart = async (req, res) => {};
+
+
+const addToCart = async (req, res) => {
+  try {
+    console.log('req.body.userId: ', req.body.userId);
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Added to cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // Remove items from user cart
 const removeFromCart = async (req, res) => {};
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,24 @@
 import jwt from "jsonwebtoken";
 
-const authMiddleware = async (req, res, next) => {};
+const authMiddleware = async (req, res, next) => {
+  const { token } = req.headers;
+
+  if (!token) {
+    return res.json({
+      success: false,
+      message: "Not Authorized please login again",
+    });
+  }
+
+  try {
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("token_decode: ", token_decode);
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 export default authMiddleware;
